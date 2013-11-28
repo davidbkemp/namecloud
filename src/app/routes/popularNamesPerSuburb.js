5af1ba31a9359eccae39a185e44c9afba1057d76
David Kemp <davidbkemp@gmail.com>
2013-11-28 16:09:32 +1100

    Support ability to return popular names at a suburb level for a bounding box

--- src/app/routes/popularNamesPerSuburb.js
+++ src/app/routes/popularNamesPerSuburb.js
@@ -1,6 +1,42 @@
-exports.list = function(req, res) {
-    var body = JSON.stringify({test: 'abc', foo: 'he hhe he'});
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Content-Length', body.length);
-    res.end(body);
-};
+(function () {
+
+    var MongoClient = require('mongodb').MongoClient;
+
+
+    var popularNamesPerSuburb = function(req, res) {
+
+        console.log(req.query);
+
+        var lon1 = parseFloat(req.query.lon1, 10),
+            lat1 = parseFloat(req.query.lat1, 10),
+            lon2 = parseFloat(req.query.lon2, 10),
+            lat2 = parseFloat(req.query.lat2, 10)
+
+        var box = [ [lon1,  lat1], [lon2, lat2] ];
+
+        MongoClient.connect('mongodb://127.0.0.1:27017/test', function(err, db) {
+            if (err) throw err;
+
+            console.log(box)
+            var collection = db
+                .collection('suburbPopularName')
+                .find({geo: {'$geoWithin': {'$box': box}}})
+                .toArray(function(err, docs) {
+                    if (err) throw err;
+                    console.dir(docs);
+                    var body = JSON.stringify(docs);
+                    res.setHeader('Content-Type', 'application/json');
+                    res.setHeader('Content-Length', body.length);
+                    res.end(body);
+                });
+
+
+        });
+
+
+    }
+    exports.list = function(req, res) {
+        popularNamesPerSuburb(req, res);
+    };
+
+})();
--- src/mongoScripts/addGeocodesForPostcodes.js
+++ src/mongoScripts/addGeocodesForPostcodes.js
@@ -2,10 +2,10 @@
 db.suburbPopularName.find().forEach(function (suburbPopularName) {
     print(suburbPopularName.postcode);
     db.suburbs.find({postcode: suburbPopularName.postcode}).forEach(function (suburb) {
-        suburbPopularName.geo = {
-            lon: suburb.centrePoint[0],
-            lat: suburb.centrePoint[1]
-        };
+        suburbPopularName.geo = [
+            suburb.centrePoint[0],
+            suburb.centrePoint[1]
+        ]
         db.suburbPopularName.save(suburbPopularName);
     });
 });
