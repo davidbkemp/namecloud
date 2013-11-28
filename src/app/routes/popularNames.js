c1c425520e1c6a4d5f61d2f05becbac48d72c4f1
David Kemp <davidbkemp@gmail.com>
2013-11-29 09:47:35 +1100

    Add all of the states

--- src/app/routes/popularNames.js
+++ src/app/routes/popularNames.js
@@ -7,11 +7,13 @@
 
     var popularNamesPerState = function(req, res) {
         var body = JSON.stringify([
-            {name: 'Smith', geo: {lat: -37.716, lon: 144.891}},
-            {name: 'Jones', geo: {lat: -41.218, lon: 1446.188}},
-            {name: 'Nyuan', geo: {lat: -25.173, lon: 144.891}},
-            {name: 'Potter', geo: {lat: -33.390, lon: 149.547}},
-            {name: 'Brown', geo: {lat: -28.101, lon: 115.192}}
+            {name: 'Smith', geo: {lat: -36.64, lon: 144.11}},
+            {name: 'Smith', geo: {lat: -31.0, lon: 146.4}},
+            {name: 'Smith', geo: {lat: -24.9, lon: 145.0}},
+            {name: 'Smith', geo: {lat: -14.5, lon: 131.7}},
+            {name: 'Smith', geo: {lat: -27.9, lon: 134.3}},
+            {name: 'Smith', geo: {lat: -42.39, lon: 146.73}},
+            {name: 'Smith', geo: {lat: -28.1, lon: 119.3}}
         ]);
         res.setHeader('Content-Type', 'application/json');
         res.setHeader('Content-Length', body.length);
